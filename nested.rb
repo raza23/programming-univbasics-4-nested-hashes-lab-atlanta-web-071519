e87a6660d9629e50b1f9e756047a7dce987be986
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-08 04:59:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
    alans_new_info = "GUI"
 
-return programmer_hash[:alans_kay][:known_for] => alans_new_info
+return programmer_hash[:alans_kay][:known_for] => [alans_new_info]
 end
 
 def adding_to_dennis
