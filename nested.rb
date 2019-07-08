b5db843c1339f93ce2dcd421af820e1f1a4edd2c
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-08 04:46:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,11 +62,12 @@ return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+# add the following information to the top level of 
+programmer_hash =
+ :yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }
 
 	programmer_hash =
  		{
@@ -83,7 +84,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash = {:yukihiro_matsumoto},{known_for: "Ruby"},{languages:"["LISP","C"]}
 
 end
 
