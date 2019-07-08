2fcb45957325a1b670cb6527bde58e9298d88044
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-08 05:51:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,6 @@ def adding_to_dennis
         }
      }
 
-return programmer_hash[:dennis_ritchie][:languages].unshift( "Assembly")
+ 	 programmer_hash[:dennis_ritchie][:languages].unshift( "Assembly")
+	 return programmer_hash
 end
